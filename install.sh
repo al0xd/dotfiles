7ab1f54adcbf20c890b24a0caa7ea0599190b2e3
Alex <dinhhungvn@gmail.com>
2022-06-20 22:25:52 +0700

    remove old plugins

--- install.sh
+++ install.sh
@@ -4,13 +4,8 @@
 unamestr=$(uname)
 zshrc_file="$HOME/.zshrc"
 vimrc_file="$HOME/.vimrc"
-vimbundle_file="$HOME/.vimrc.bundles"
-tmux_file="$HOME/.tmux.conf"
-eslint_file="$HOME/.eslintrc.yml"
-stylelint_file="$HOME/.stylelintrc.yml"
-gitconfig_file="$HOME/.gitconfig"
-nvimconfig_file="$HOME/.config/nvim/init.vim"
-set -e
+tmuxfile="$HOME/.tmux.conf"
+set -
 # Install Packages
 function insp(){
   # Config Symbol Links
@@ -18,72 +13,18 @@ function insp(){
     # Backup Old zshrc file
     mv ~/.zshrc $HOME/.zshrc.backup
   fi
-  if [ -f "$vimrc_file" ]; then
-    mv $vimrc_file $HOME/.vimrc.backup
-  fi
-  if [ -f "$vimbundle_file" ]; then
-    mv $vimbundle_file $HOME/.vimrc.bundles.backup
-  fi
   if [ -f "$tmux_file" ]; then
     mv $tmux_file $HOME/.tmux.conf.backup
-  fi
-  if [ -f "$eslint_file" ]; then
-    mv $eslint_file $HOME/.eslintrc.yml.backup
-  fi
-  if [ -f "$stylelint_file" ]; then
-    mv $stylelint_file $HOME/.stylelintrc.yml.backup
-  fi
-  if [ ! -d "$HOME/.config" ]; then
-    mkdir "$HOME/.config"
-  fi
-
-  if [ -d "$HOME/.config/nvim" ]; then
-    mv "$HOME/.config/nvim" "$HOME/.config/nvim.backup"
-  fi
-  mkdir "$HOME/.config/nvim"
-
-  if [ -f "$gitconfig_file" ]; then
-    mv $gitconfig_file $HOME/.gitconfig.backup
   fi
 
 
 
   # Create Symbolinks
   ln -s $HOME/.dotfiles/zsh/zshrc $zshrc_file
-  ln -s $HOME/.dotfiles/vim/vimrc $vimrc_file
-  ln -s $HOME/.dotfiles/vim/vimrc.bundles $vimbundle_file
   ln -s $HOME/.dotfiles/tmux/tmux.conf $tmux_file
-  ln -s $HOME/.dotfiles/linter/eslintrc.yml $eslint_file
-  ln -s $HOME/.dotfiles/linter/stylelintrc.yml $stylelint_file
-  ln -s $HOME/.dotfiles/git/gitconfig $gitconfig_file
-  ln -s $HOME/.dotfiles/vim/vimrc $nvimconfig_file
 
 
-  # Install zsh plugins
-  brew bundle
-  # Install Ruby Bundle
-  if ! hash bundle 2>/dev/null; then
-    gem install bundler
-  fi
-  bundle install
-  # Install Vim Plug
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  # Install npm packages
-  npm install -g
 }
-# End install function
-# Start Install Script
-# Check Brew exists?
-if ! hash brew 2>/dev/null; then
-  printf "Please Install HomeBrew First\n"
-  exit $?
-fi
-
-
-  # Install vim-plug
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Check .dotfiles has been installed
 if [ ! -d "$HOME/.dotfiles" ]; then

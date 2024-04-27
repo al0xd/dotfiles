b21e2617a2ec3b437c6f27be7a9ddedaa39a8011
Alex <dinhhungvn@gmail.com>
2024-04-27 17:47:31 +0700

    update lunavim

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -57,6 +57,38 @@ lvim.plugins = {
   },
   {
     "kristijanhusak/vim-carbon-now-sh"
+  },
+  {
+    "ibhagwan/fzf-lua"
+  },
+  {
+  -- AI
+    "Bryley/neoai.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+    cmd = {
+        "NeoAI",
+        "NeoAIOpen",
+        "NeoAIClose",
+        "NeoAIToggle",
+        "NeoAIContext",
+        "NeoAIContextOpen",
+        "NeoAIContextClose",
+        "NeoAIInject",
+        "NeoAIInjectCode",
+        "NeoAIInjectContext",
+        "NeoAIInjectContextCode",
+    },
+    keys = {
+        { "<leader>as", desc = "summarize text" },
+        { "<leader>ag", desc = "generate git message" },
+    },
+    config = function()
+        require("neoai").setup({
+            -- Options go here
+        })
+    end
   }
   -- {
   --   "ethanholz/nvim-lastplace",

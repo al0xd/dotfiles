d74785a5dae1be6edbfebb14bb8a3237937d5127
Alex <dinhhungvn@gmail.com>
2024-06-08 23:08:42 +0700

    Add ale plugin

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -288,6 +288,23 @@ lvim.plugins = {
     "ibhagwan/fzf-lua"
   },
   {
+      'dense-analysis/ale',
+      config = function()
+          -- Configuration goes here.
+          local g = vim.g
+
+          g.ale_ruby_rubocop_auto_correct_all = 1
+
+          g.ale_linters = {
+              ruby = {'rubocop', 'ruby'},
+              lua = {'lua_language_server'}
+          }
+      end
+  },
+  {
+    'neoclide/coc.nvim'
+  },
+  {
   -- AI
     "Bryley/neoai.nvim",
     dependencies = {
